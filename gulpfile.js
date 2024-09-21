c3e06ce45605d3bf195bf64fa65cb0c9e7ad487b
Gabriel Munhak <heclypso@gmail.com>
2024-09-21 12:30:34 -0300

    Ajustado gulpfile para que seja possível a visualização do projeto pela plataforma vercel

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@ const sass = require("gulp-sass")(require("sass"));
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles);
-exports.watch = function() {
-    gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
+function html() {
+    return gulp.src('./src/*.html')
+        .pipe(gulp.dest('./dist'));
 }
+
+exports.default = gulp.parallel(styles, html);
+exports.watch = function() {
+    gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+    gulp.watch("./src/*.html", gulp.parallel(html)); 
+};
