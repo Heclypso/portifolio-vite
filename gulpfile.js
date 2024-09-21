2db352e617a0e9dc2429f678d7d2895fb0cd4ec4
Gabriel Munhak <heclypso@gmail.com>
2024-09-21 13:08:19 -0300

    Corrigido erro relacionado ao carregamento do css da página

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,22 @@ const sass = require("gulp-sass")(require("sass"));
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
-}
-
-function html() {
-    return gulp.src('./src/*.html')
         .pipe(gulp.dest('./dist'));
 }
 
-exports.default = gulp.parallel(styles, html);
+function html() {
+    return gulp.src('*.html')
+        .pipe(gulp.dest('./dist'));
+}
+
+function javascript() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(gulp.dest('./dist'));
+}
+
+exports.default = gulp.parallel(styles, html, javascript);
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
-    gulp.watch("./src/*.html", gulp.parallel(html)); 
+    gulp.watch("*.html", gulp.parallel(html)); 
+    gulp.watch("./src/scripts/*.js", gulp.parallel(javascript)); 
 };
